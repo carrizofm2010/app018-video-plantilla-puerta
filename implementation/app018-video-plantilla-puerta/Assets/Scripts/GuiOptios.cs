90486455f1fa254d154db9179631d8a156c51f62
enriqif@gmail.com <enriqif@gmail.com>
2015-01-16 20:29:55 -0300

    actualizacion

--- implementation/app018-video-plantilla-puerta/Assets/Scripts/GuiOptios.cs
+++ implementation/app018-video-plantilla-puerta/Assets/Scripts/GuiOptios.cs
@@ -4,17 +4,36 @@ using System.Collections;
 public class GuiOptios : MonoBehaviour {
 	public GUIStyle styleBox;
 	private Rect cajaConfiguracionServidor;
+	private bool flagScreem;
+	private bool flagExit;
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
+		flagScreem=false;
+		flagExit=false;
+	}	
 	// Update is called once per frame
 	void Update () {
 		this.redimensionarPantalla(Screen.width, Screen.height);
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			if(flagExit){
+				flagScreem=false;
+				flagExit=false;
+			}else{
+				Application.Quit();
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.Menu))
+		{
+			flagScreem=true;
+			flagExit=true;
+		}
 	}
 	void OnGUI() {
-		GUI.Box(cajaConfiguracionServidor,AppIUConstantes.CADENA_VACIA, styleBox);
+		if(flagScreem){
+			GUI.Box(cajaConfiguracionServidor,AppIUConstantes.CADENA_VACIA, styleBox);
+		}
+
 	}
 	public void redimensionarPantalla(float width, float height ){
 		cajaConfiguracionServidor=new Rect(0,100*height/100-20*height/100,100*width/100,20*height/100);
