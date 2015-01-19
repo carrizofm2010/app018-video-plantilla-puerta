fa74733555ca5482f415f428d1c7b049fee56d6e
Walter <walterjesusvale@gmail.com>
2015-01-19 13:23:11 -0200

    flash funcional

--- implementation/app018-video-plantilla-puerta/Assets/Scripts/GuiOptios.cs
+++ implementation/app018-video-plantilla-puerta/Assets/Scripts/GuiOptios.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class GuiOptios : MonoBehaviour {
 	public GUIStyle styleBox;
+	public GUIStyle styleBotonFlash;
+	public GUIStyle styleBotonAbout;
 	private Rect cajaConfiguracionServidor;
+	private Rect cajaBotonFlash;
+	private Rect cajaBotonAbout;
 	private bool flagScreem;
 	private bool flagExit;
+	VideoPlaybackUIEventHandler handler;
 	// Use this for initialization
 	void Start () {
 		flagScreem=false;
 		flagExit=false;
+		handler = GameObject.FindObjectOfType(typeof(VideoPlaybackUIEventHandler)) as VideoPlaybackUIEventHandler;
 	}	
 	// Update is called once per frame
 	void Update () {
@@ -32,10 +38,28 @@ public class GuiOptios : MonoBehaviour {
 	void OnGUI() {
 		if(flagScreem){
 			GUI.Box(cajaConfiguracionServidor,AppIUConstantes.CADENA_VACIA, styleBox);
+			if(GUI.Button(cajaBotonFlash,AppIUConstantes.CADENA_VACIA,styleBotonFlash)){
+				CameraDevice.Instance.SetFlashTorchMode(true);
+//				if (handler != null)
+//				{
+//					handler.View.mCameraFlashSettings.Enable(true);
+//					CameraDevice.Instance.SetFlashTorchMode(true);
+//				}
+			}
+			if(GUI.Button(cajaBotonAbout,AppIUConstantes.CADENA_VACIA,styleBotonAbout)){
+				CameraDevice.Instance.SetFlashTorchMode(false);
+//				if (handler != null)
+//				{
+//					handler.View.mCameraFlashSettings.Enable(false);
+//				}
+			}
 		}
 
 	}
+	
 	public void redimensionarPantalla(float width, float height ){
 		cajaConfiguracionServidor=new Rect(0,100*height/100-20*height/100,100*width/100,20*height/100);
+		cajaBotonFlash=new Rect(10*width/100,100*height/100-19*height/100,30*width/100,30*width/100);
+		cajaBotonAbout=new Rect(100*width/100-30*width/100-10*width/100,100*height/100-19*height/100,30*width/100,30*width/100);
 	}
 }
