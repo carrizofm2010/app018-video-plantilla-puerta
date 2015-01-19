0ee25fb21d408661be44f8f24cff92b03c827e99
Walter <walterjesusvale@gmail.com>
2015-01-19 15:31:05 -0200

    actualizacion Scripts/GuiOptios.cs

--- implementation/app018-video-plantilla-puerta/Assets/Scripts/AppIUConstantes.cs
+++ implementation/app018-video-plantilla-puerta/Assets/Scripts/AppIUConstantes.cs
@@ -3,4 +3,5 @@ using System.Collections;
 /*Define todas las constantes utilizas en la aplicacion*/
 public class AppIUConstantes{
 	public static string CADENA_VACIA="";
+	public static float TIEMPO_ESPERA=0.5f;
 }
