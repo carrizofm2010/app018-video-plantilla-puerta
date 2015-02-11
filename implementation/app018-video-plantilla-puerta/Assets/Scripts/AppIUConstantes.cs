691e684d9ba1a424b700857ad63f1d28cfed4967
Walter <walterjesusvale@gmail.com>
2015-02-10 23:50:41 -0300

    Eliminacion proyecto viejo, para poner el nuevo

--- implementation/app018-video-plantilla-puerta/Assets/Scripts/AppIUConstantes.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-/*Define todas las constantes utilizas en la aplicacion*/
-public class AppIUConstantes{
-	public static string CADENA_VACIA="";
-	public static float TIEMPO_ESPERA=0.5f;
-}
